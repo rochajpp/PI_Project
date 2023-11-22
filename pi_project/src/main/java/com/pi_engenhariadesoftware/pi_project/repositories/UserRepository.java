dcfe3e0379016c29d7236236c1d0960ba745fc05
Lucas Venicio <vinicioslucas3@gmail.com>
2023-11-22 19:06:07 -0300

    Criando e configurando os repositórios

---
+++ pi_project/src/main/java/com/pi_engenhariadesoftware/pi_project/repositories/UserRepository.java
@@ -0,0 +1,24 @@
+package com.pi_engenhariadesoftware.pi_project.repositories;
+
+import java.time.LocalDate;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.pi_engenhariadesoftware.pi_project.entities.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+    @Query(value="SELECT * FROM user WHERE login = :login AND password = :password", nativeQuery = true)
+    public User login(String login, String password);
+
+    @Query(value="SELECT * FROM user WHERE login = :login AND birthdate = :birthdate", nativeQuery = true)
+    public User reset(String login, LocalDate birthdate);
+
+    @Query(value="SELECT * FROM user WHERE id = :idUser", nativeQuery = true)
+    public User getUser(int idUser);
+
+    @Query(value="SELECT * FROM user WHERE login = :login", nativeQuery = true)
+    public User checkLogin(String login);
+}
