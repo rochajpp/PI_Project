427cbc6be4d38922b8b0d30efeb7cf4d4bce9944
João Paulo <joao.rochap03@gmail.com>
2023-11-23 14:04:03 -0300

    Adicionando e configurando sistema de Interceptor

---
+++ pi_project/src/main/java/com/pi_engenhariadesoftware/pi_project/Services/Authentication/InterceptorAppConfig.java
@@ -0,0 +1,20 @@
+package com.pi_engenhariadesoftware.pi_project.Services.Authentication;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class InterceptorAppConfig implements WebMvcConfigurer{
+    
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(new LoginInterceptor())
+        .addPathPatterns("/admin", "/setCar", "/rmCar", "/backCar", "/addCar", "/addUser", "/alterUser", "/removeUser");
+        
+        registry.addInterceptor(new ResetPassInterceptor())
+        .addPathPatterns("/setPassForm");
+        
+    }
+}
